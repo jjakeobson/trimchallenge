967924c4b0272334a17a427ac29171edf71306c0
jjakeobson <jjakeobson@gmail.com>
2015-12-17 18:54:42 -1000

    finalllll

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,6 +3,6 @@ class ContactMailer < ApplicationMailer
 
    def send_email(user)
     @user = user
-    mail(to: "jakeejacobson@gmail.com", from: "jakeejacobson@gmail.com", subject: "#{user.name}'s Trim Challenge")
+    mail(to: "tim@trimagency.com", from: "jakeejacobson@gmail.com", subject: "#{user.name}'s Trim Challenge")
   end
 end
